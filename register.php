29bf719a58af0f06c0db6c6152f1ed05e42ea720
Mark Anthony A. Tavera <markanthony.tavera@benilde.edu.ph>
2017-01-23 20:42:15 +0800

    Update #3
    Changed Welcome.php Layout

--- register.php
+++ register.php
@@ -8,41 +8,38 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 	<link rel="stylesheet" href="Style.css">
 	</header>
-<body>
+<body background="http://www.benilde.edu.ph/img/bg_02.jpg">
 	<div class="container bgimg w3-display-center">
-		<div style="text-align:center;">
-			<br/>
-			<br/>
-			<br/>
-			<br/>
-			<br/>
-			<br/>
-			<p><b>De LaSalle College Of Saint Benilde</b><p>
+		<div style="text-align:center;"><br/>
+			<p><img src="http://www.benilde.edu.ph/img/seal-trans.png" style="width:200px;"><p>
 			<p><b>Student Registration</b></p>
 		</div>
 		<div style="text-align:center">
-			<p><button onclick="document.getElementById('contact').style.display='block'" class="w3-btn w3-hover-light-green">Click Here to Register</button></p>
+			<p><button onclick="document.getElementById('contact').style.display='block'" class="w3-btn w3-hover-white" style="background-color:darkgreen;" >Register Here</button></p>
 		</div>
 	</div>
-	
+	<div class="w3-display-bottomleft w3-padding-jumbo">
+		<p class="w3-medium" style="color:green;"><b>&copy; Mark Anthony A. Tavera</b></p>
+		<p class="w3-medium" style="color:green;"><b>De La Salle College of Saint Benilde</b></p>
+	</div>
 	<div id="contact" class="w3-modal">
 		<div class="w3-modal-content w3-animate-zoom">
 			<div class="w3-container w3-green">
-				<span onclick="document.getElementById('contact').style.display='none'" class="w3-closebtn w3-xxlarge w3-hover-text-grey">x</span>
+				<span onclick="document.getElementById('contact').style.display='none'" class="w3-closebtn w3-xxlarge w3-hover-text-white">x</span>
 					<h1>Register Form</h1>
-				</div>
+			</div>
 				<div class="w3-container">
-					<p style="font-size:20px;">Please Fill up all necessary information...</p>
+					<p style="font-size:20px;">Please fill in all necessary information...</p>
 					<form method="POST" action="welcome.php">
 					<p style="font-size:15px;"><input name="sid" class="w3-input w3-padding-10 w3-border" type="text" placeholder="StudentID" required /></p>
-					<p style="font-size:15px;"><input name="ln" class="w3-input w3-padding-10 w3-border" type="text" placeholder="Last Name" required /></p>
 					<p style="font-size:15px;"><input name="fn" class="w3-input w3-padding-10 w3-border" type="text" placeholder="First Name" required /></p>
+					<p style="font-size:15px;"><input name="ln" class="w3-input w3-padding-10 w3-border" type="text" placeholder="Last Name" required /></p>
 					<p style="font-size:15px;"><input name="email" class="w3-input w3-padding-10 w3-border" type="email" placeholder="Email Address" required /></p>
 					<p style="font-size:15px;"><input name="pwd" class="w3-input w3-padding-10 w3-border" type="password" placeholder="Password" required /></p>
-					<p style="font-size:15px;"><input name="bday" class="w3-input w3-padding-10 w3-border" type="date" placeholder="Birthdate" required /></p>
+					<p style="font-size:20px;"><h4>Birth Date:</h4><input name="bday" class="w3-input w3-padding-10 w3-border" type="date" placeholder="BirthDate" required /></p>
 					<p style="font-size:15px; text-align:center;"><button name="register" class="w3-btn w3-light-green w3-padding" type="submit" />Register</button></p>
 				</div>
-			</div>
 		</div>
+	</div>
 </body>
 </html>
--- welcome.php
+++ welcome.php
@@ -1,22 +1,77 @@
 <?php
 	if (isset($_POST['register'])) {
 		$studentNo = $_POST['sid'];
-		$lastName = $_POST['ln'];
 		$firstName = $_POST['fn'];
+		$lastName = $_POST['ln'];
 		$emailAddress = $_POST['email'];
 		$password = $_POST['pwd'];
 		$birthDate = $_POST['bday'];
-
-		echo "<b>Welcome, " . $lastName . " " . $firstName . "!</b><br/><br/>" . 
-		"<b>Your ID number is: </b>" . $studentNo . "!</br>" . 
-		"<b>Your Email Address is: </b>" . $emailAddress . "! </br>" .
-		"<b>Your Password is: </b>". $password . "! </br>" .
-		"<b>Your Birthday is: </b>". $birthDate . "! </br><br/>" . 
-		"<b>The Time is: </b>" . date('c');
-
+	
+		//echo "<b>Welcome, " . $lastName . " " . $firstName . "!</b><br/><br/>" . 
+		//"<b>Your ID number is: </b>" . $studentNo . "!</br>" . 
+		//"<b>Your Email Address is: </b>" . $emailAddress . "! </br>" .
+		//"<b>Your Password is: </b>". $password . "! </br>" .
+		//"<b>Your Birthday is: </b>". $birthDate . "! </br><br/>" . 
+		//"<b>The Time is: </b>" . date('c');
+	
 	} 
 	else {
 		header('location: register.php');
 
 	}
 ?>
+<html>
+	<title>MyAccount</title>
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+	<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+<body>
+	<center><div class="w3-content w3-margin-center" style="max-width:1400px;">
+	<div class="w3-row-padding">
+		<div class="w3-third">
+			<div class="w3-white w3-text-grey w3-card-4">
+				<div class="w3-display-container">
+					<img src="https://cdn3.iconfinder.com/data/icons/avatar-set/512/Avatar02-512.png" style="width:100%" alt="Avatar" />
+				<div class="w3-display-bottomleft w3-container w3-text-black">
+					<h2><?php echo $firstName . " " . $lastName ?></h2>
+				</div>
+				</div>
+			<div class="w3-container">
+				<p><i class="fa fa-graduation-cap fa-fw w3-margin-left w3-large w3-text-teal"></i> De La Salle College of Saint Benilde</p>
+				<p><i class="fa fa-map-marker fa-fw w3-margin-left w3-large w3-text-teal"></i> 2544 Taft Ave. Malate, Manila, Philippines</p>
+				<p><i class="fa fa-envelope fa-fw w3-margin-left w3-large w3-text-teal"></i> www.benilde.edu.ph</p>
+				<p><i class="fa fa-phone fa-fw w3-margin-left w3-large w3-text-teal"></i> local 1325 or 1322</p>
+				<h2 class="w3-text-grey w3-padding-16"><i class="fa fa-star fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Welcome <?php echo $firstName . " " . $lastName ?></h2>
+			<div class="w3-container">
+				<h5 class="w3-opacity"><b>Student Information</b></h5>
+				<p><b>Student ID No.:</b> <?php echo $studentNo?></p>
+				<p><b>First Name:</b> <?php echo $firstName?></p>
+				<p><b>Last Name:</b> <?php echo $lastName?></p>
+				<p><b>Email Address:</b> <?php echo $emailAddress?></p>
+				<p><b>Birth Date:</b> <?php echo $birthDate?></p>
+			</div>
+			</div>
+			</div>
+		</div>
+	</center></div>
+	<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-text-indigo w3-large"></i>
+  <i class="fa fa-instagram w3-hover-text-purple w3-large"></i>
+  <i class="fa fa-snapchat w3-hover-text-yellow w3-large"></i>
+  <i class="fa fa-pinterest-p w3-hover-text-red w3-large"></i>
+  <i class="fa fa-twitter w3-hover-text-light-blue w3-large"></i>
+  <i class="fa fa-linkedin w3-hover-text-indigo w3-large"></i>
+  <p>Powered by <a href="http://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+	</body>
+</html>
+				
+				
+				
+				
